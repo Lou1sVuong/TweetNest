6a38637c5a41df85da89c2c588d77c954ebc541b
Lou1sVuong <xuannvuongg@gmail.com>
2024-04-28 02:12:40 +0700

    add more error message in validator

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,3 +1,16 @@
 export const USERS_MESSAGES = {
-  VALIDATION_ERROR: 'Validation error'
+  VALIDATION_ERROR: 'Validation error',
+  NAME_REQUIRED: 'Name is required',
+  NAME_MUST_BE_STRING: 'Name must be a string',
+  NAME_LENGTH: 'Name must be between 1 and 100 characters long',
+  EMAIL_ALREADY_EXIST: 'Email is already exist',
+  EMAIL_REQUIRED: 'Email is required',
+  EMAIL_INVALID: 'Email is invalid',
+  PASSWORD_REQUIRED: 'Password is required',
+  PASSWORD_MUST_BE_STRING: 'Password must be a string',
+  PASSWORD_LENGTH: 'Password must be at least 6 characters long',
+  PASSWORD_MUST_BE_STRONG:
+    ' Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, one number and one symbol',
+  CONFIRM_PASSWORD_REQUIRED: 'Confirm password is required',
+  CONFIRM_PASSWORD_MUST_BE_MATCH: 'Confirm password must match with password'
 } as const
--- src/middlewares/users.middlewares.ts
+++ src/middlewares/users.middlewares.ts
@@ -1,5 +1,6 @@
 import { error } from 'console'
 import { checkSchema } from 'express-validator'
+import { USERS_MESSAGES } from '~/constants/messages'
 import { ErrorWithStatus } from '~/models/errors'
 import usersService from '~/services/users.services'
 import { validate } from '~/utils/validation.utils'
@@ -22,39 +23,52 @@ export const loginValidation = validate(
 export const registerValidation = validate(
   checkSchema({
     name: {
-      notEmpty: true,
-      isString: true,
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.NAME_REQUIRED
+      },
+      isString: {
+        errorMessage: USERS_MESSAGES.NAME_MUST_BE_STRING
+      },
       isLength: {
         options: {
           min: 1,
           max: 100
         }
       },
+      errorMessage: USERS_MESSAGES.NAME_LENGTH,
       trim: true
     },
     email: {
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.EMAIL_REQUIRED
+      },
       trim: true,
-      isEmail: true,
+      isEmail: {
+        errorMessage: USERS_MESSAGES.EMAIL_INVALID
+      },
       custom: {
         options: async (value) => {
           const isEmailExist = await usersService.checkEmailExist(value)
           if (isEmailExist) {
-            throw new Error('Email is already exist')
+            throw new Error(USERS_MESSAGES.EMAIL_ALREADY_EXIST)
           }
           return !isEmailExist
         }
       }
     },
     password: {
-      isString: true,
-      notEmpty: true,
+      isString: {
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRING
+      },
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.PASSWORD_REQUIRED
+      },
       isLength: {
         options: {
           min: 6,
           max: 50
         },
-        errorMessage: 'Password must be at least 6 characters long'
+        errorMessage: USERS_MESSAGES.PASSWORD_LENGTH
       },
       trim: true,
       isStrongPassword: {
@@ -65,18 +79,19 @@ export const registerValidation = validate(
           minNumbers: 1,
           minSymbols: 1
         },
-        errorMessage:
-          'Password must be at least 6 characters long, contain at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 symbol'
+        errorMessage: USERS_MESSAGES.PASSWORD_MUST_BE_STRONG
       }
     },
     confirm_password: {
       isString: true,
-      notEmpty: true,
+      notEmpty: {
+        errorMessage: USERS_MESSAGES.CONFIRM_PASSWORD_REQUIRED
+      },
       trim: true,
       custom: {
         options: (value, { req }) => {
           if (value !== req.body.password) {
-            throw new Error('Password confirm do not match password')
+            throw new Error(USERS_MESSAGES.CONFIRM_PASSWORD_MUST_BE_MATCH)
           }
           return value
         }
