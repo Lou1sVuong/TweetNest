0c6946b448cb6d30d8a4c51790f255367e017479
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-05-26 15:16:52 +0700

    refactor : Refactor error handling middleware

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -2,15 +2,31 @@ import { Request, Response, NextFunction } from 'express'
 import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/errors'
+
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof ErrorWithStatus) {
-    return res.status(err.status).json(omit(err, 'status'))
+  try {
+    if (err instanceof ErrorWithStatus) {
+      return res.status(err.status).json(omit(err, 'status'))
+    }
+
+    const finalError: any = {}
+    Object.getOwnPropertyNames(err).forEach((key) => {
+      if (
+        !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+        !Object.getOwnPropertyDescriptor(err, key)?.writable
+      ) {
+        return (finalError[key] = err[key])
+      }
+    })
+
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: finalError.message,
+      errorInfo: omit(finalError, ['stack'])
+    })
+  } catch (error) {
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      message: 'Internal server error',
+      errorInfo: {}
+    })
   }
-  Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
-  })
-  res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, 'stack')
-  })
 }
