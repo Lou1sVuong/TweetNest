fc49e7ca785b4bd3f46a47770217e0b0bf5abf93
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-05-29 21:34:51 +0700

    fix : circular structures error in error.middlewares.ts chore: Update AWS S3 credentials in .env file

--- src/middlewares/error.middlewares.ts
+++ src/middlewares/error.middlewares.ts
@@ -2,15 +2,40 @@ import { Request, Response, NextFunction } from 'express'
 import { omit } from 'lodash'
 import HTTP_STATUS from '~/constants/httpStatus'
 import { ErrorWithStatus } from '~/models/errors'
+
+// Helper function to handle circular structures
+function getCircularReplacer() {
+  const seen = new WeakSet()
+  return (key: string, value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return
+      }
+      seen.add(value)
+    }
+    return value
+  }
+}
+
 export const defaultErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ErrorWithStatus) {
     return res.status(err.status).json(omit(err, 'status'))
   }
+
+  const finalError: any = {}
   Object.getOwnPropertyNames(err).forEach((key) => {
-    Object.defineProperty(err, key, { enumerable: true })
+    if (
+      !Object.getOwnPropertyDescriptor(err, key)?.configurable ||
+      !Object.getOwnPropertyDescriptor(err, key)?.writable
+    ) {
+      return (finalError[key] = err[key])
+    }
   })
+
+  const safeError = JSON.stringify(finalError, getCircularReplacer())
+
   res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
-    message: err.message,
-    errorInfo: omit(err, 'stack')
+    message: finalError.message,
+    errorInfo: omit(JSON.parse(safeError), ['stack']) // Parsing back to object to use `omit`
   })
 }
