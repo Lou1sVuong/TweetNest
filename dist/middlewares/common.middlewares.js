790d17f7f4c838072a0ae3aa134a8d63b7f4fd92
Mai V. Xuan Vuong <xuannvuongg@gmail.com>
2024-06-23 18:58:35 +0700

    refactor: Remove unused files and update gitignore

--- dist/middlewares/common.middlewares.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.filterMiddleware = void 0;
-const lodash_1 = require("lodash");
-const filterMiddleware = (filterKeys) => (req, res, next) => {
-    req.body = (0, lodash_1.pick)(req.body, filterKeys);
-    next();
-};
-exports.filterMiddleware = filterMiddleware;
--- dist/utils/commons.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.numberEnumToArray = void 0;
-const numberEnumToArray = (numberEnum) => {
-    return Object.values(numberEnum).filter((value) => typeof value === 'number');
-};
-exports.numberEnumToArray = numberEnumToArray;
